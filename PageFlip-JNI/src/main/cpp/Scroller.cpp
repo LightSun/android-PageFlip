82f017380a4703cbc5edf0840fe7db73b98e5f0b
eschao <esc.chao@gmail.com>
2016-12-06 00:35:24 +0800

    Add Scroller class

--- PageFlip-JNI/src/main/cpp/Scroller.cpp
+++ PageFlip-JNI/src/main/cpp/Scroller.cpp
@@ -1,5 +1,71 @@
-//
-// Created by Chao Zhang on 05/12/2016.
-//
+/*
+ * Copyright (C) 2016 eschao <esc.chao@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 #include "Scroller.h"
+
+Scroller::Scroller()
+        : m_finished(true)
+{
+    m_interpolator = new LinearInterpolator();
+}
+
+Scroller::Scroller(Interpolator *interpolator)
+        : m_finished(true), m_interpolator(interpolator)
+{
+}
+
+Scroller::~Scroller()
+{
+    if (m_interpolator) {
+        delete m_interpolator;
+    }
+}
+
+bool Scroller::compute_scroll_offset()
+{
+    if (m_finished) {
+        return false;
+    }
+
+    long time_passed = get_system_clock() - m_start_time;
+    if (time_passed < m_duration) {
+        const float x = m_interpolator->interpolate( time_passed * m_duration);
+        m_curr_x = (float)(m_start_x + round(x * m_delta_x));
+        m_curr_y = (float)(m_start_y + round(x * m_delta_y));
+    }
+    else {
+        m_curr_x = m_final_x;
+        m_curr_y = m_final_y;
+        m_finished = true;
+    }
+
+    return true;
+}
+
+void Scroller::start_scroll(float start_x, float start_y, float dx, float dy,
+                            int duration)
+{
+    m_finished = false;
+    m_duration = duration;
+    m_start_time = get_system_clock();
+
+    m_start_x = start_x;
+    m_start_y = start_y;
+    m_final_x = start_x + dx;
+    m_final_y = start_y + dy;
+    m_delta_x = dx;
+    m_delta_y = dy;
+}
--- PageFlip-JNI/src/main/cpp/Scroller.h
+++ PageFlip-JNI/src/main/cpp/Scroller.h
@@ -1,12 +1,68 @@
-//
-// Created by Chao Zhang on 05/12/2016.
-//
+/*
+ * Copyright (C) 2016 eschao <esc.chao@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 #ifndef ANDROID_PAGEFLIP_SCROLLER_H
 #define ANDROID_PAGEFLIP_SCROLLER_H
 
+#include <math.h>
+#include <time.h>
+
+#define DEFAULT_DURATION 250
+
 class Interpolator {
 
+public:
+    virtual ~Interpolator();
+
+    virtual float interpolate(float input) = 0;
+
+protected:
+    Interpolator()
+    { }
+};
+
+class AccelerateInterpolator : public Interpolator {
+
+public:
+    explicit AccelerateInterpolator(float factor)
+            : m_factor(factor), m_double_factor(factor * 2)
+    { }
+
+    virtual float interpolate(float input) override
+    {
+        if (m_factor == 1.0f) {
+            return input * input;
+        }
+        else {
+            return (float)pow(input, m_double_factor);
+        }
+    }
+
+private:
+    const float m_factor;
+    const float m_double_factor;
+};
+
+class LinearInterpolator : public Interpolator {
+
+public:
+    virtual float interpolate(float input) override
+    {
+        return input;
+    }
 };
 
 /**
@@ -15,6 +71,95 @@ class Interpolator {
  */
 class Scroller {
 
+public:
+    Scroller();
+    Scroller(Interpolator *interpolator);
+    ~Scroller();
+
+    bool compute_scroll_offset();
+    void start_scroll(float start_x, float start_y, float dx, float dy,
+                      int duration = DEFAULT_DURATION);
+
+    inline void set_interpolator(Interpolator *interpolator)
+    {
+        if (m_interpolator) {
+            delete m_interpolator;
+        }
+        m_interpolator = interpolator;
+    }
+
+    inline bool is_finished()
+    {
+        return m_finished;
+    }
+
+    inline void set_finished(bool finished)
+    {
+        m_finished = finished;
+    }
+
+    inline int duration()
+    {
+        return m_duration;
+    }
+
+    inline float curr_x()
+    {
+        return m_curr_x;
+    }
+
+    inline float curr_y()
+    {
+        return m_curr_y;
+    }
+
+    inline float start_x()
+    {
+        return m_start_x;
+    }
+
+    inline float start_y()
+    {
+        return m_start_y;
+    }
+
+    inline float final_x()
+    {
+        return m_final_x;
+    }
+
+    inline float final_y()
+    {
+        return m_final_y;
+    }
+
+    inline void abortAnimation()
+    {
+        m_curr_x = m_final_x;
+        m_curr_y = m_final_y;
+        m_finished = true;
+    }
+
+private:
+    inline long get_system_clock()
+    {
+        return clock() / CLOCKS_PER_SEC * 1000;
+    }
+private:
+    Interpolator* m_interpolator;
+
+    float m_start_x;
+    float m_start_y;
+    float m_final_x;
+    float m_final_y;
+    float m_delta_x;
+    float m_delta_y;
+
+    int m_duration;
+    long m_start_time;
+    float m_curr_x;
+    float m_curr_y;
+    bool m_finished;
 };
 
 
